a5db25d5e8cd907e14fc1454ca798777f7be06e9
Tyler <tyler@echosec.net>
2022-05-05 20:53:53 -0700

    Create function to avoid body

--- src/main/java/com/battlesnake/javasnake/GameController.java
+++ src/main/java/com/battlesnake/javasnake/GameController.java
@@ -2,9 +2,9 @@ package com.battlesnake.javasnake;
 
 import java.util.*;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -44,23 +44,24 @@ public class GameController {
     @PostMapping("/move")
     public Map<String, String> move(@RequestBody JsonNode moveRequest) {
 
-        try {
-            LOG.info("Data: {}", JSON_MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(moveRequest));
-        } catch (JsonProcessingException e) {
-            LOG.error("Error parsing payload", e);
-        }
+//        try {
+//            LOG.info("Data: {}", JSON_MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(moveRequest));
+//        } catch (JsonProcessingException e) {
+//            LOG.error("Error parsing payload", e);
+//        }
 
         JsonNode head = moveRequest.get("you").get("head");
-        JsonNode body = moveRequest.get("you").get("body");
+        ArrayNode body = (ArrayNode) moveRequest.get("you").get("body");
 
         JsonNode xMax = moveRequest.get("board").get("width");
         JsonNode yMax = moveRequest.get("board").get("height");
 
         ArrayList<String> possibleMoves = new ArrayList<>(Arrays.asList("up", "down", "left", "right"));
 
-        // TODO: refactor this function to avoid entire body
-        avoidMyNeck(head, body, possibleMoves);
         avoidWalls(head, yMax.asInt(), xMax.asInt(), possibleMoves);
+        avoidMyBody(head, body, possibleMoves);
+
+        // TODO: there is a bug currently where in corners or perhaps at index 0's the snake will run into the walls/
 
         // TODO: Using information from 'moveRequest', don't let your Battlesnake pick a
         // move that would collide with another Battlesnake
@@ -69,7 +70,10 @@ public class GameController {
         // towards a piece of food on the board
 
         // Choose a random direction to move in
-        final int choice = new Random().nextInt(possibleMoves.size());
+        int choice = 0;
+        if (possibleMoves.size() > 1) {
+            choice = new Random().nextInt(possibleMoves.size());
+        }
         final String move = possibleMoves.get(choice);
 
         LOG.info("MOVE {}", move);
@@ -85,18 +89,18 @@ public class GameController {
         return EMPTY;
     }
 
-    public void avoidMyNeck(JsonNode head, JsonNode body, ArrayList<String> possibleMoves) {
-        JsonNode neck = body.get(1);
-
-        if (neck.get("x").asInt() < head.get("x").asInt()) {
-            possibleMoves.remove("left");
-        } else if (neck.get("x").asInt() > head.get("x").asInt()) {
-            possibleMoves.remove("right");
-        } else if (neck.get("y").asInt() < head.get("y").asInt()) {
-            possibleMoves.remove("down");
-        } else if (neck.get("y").asInt() > head.get("y").asInt()) {
-            possibleMoves.remove("up");
-        }
+    public void avoidMyBody(JsonNode head, ArrayNode body, ArrayList<String> possibleMoves) {
+        body.forEach(bodyPart -> {
+            if (bodyPart.get("x").asInt() < head.get("x").asInt()) {
+                possibleMoves.remove("left");
+            } else if (bodyPart.get("x").asInt() > head.get("x").asInt()) {
+                possibleMoves.remove("right");
+            } else if (bodyPart.get("y").asInt() < head.get("y").asInt()) {
+                possibleMoves.remove("down");
+            } else if (bodyPart.get("y").asInt() > head.get("y").asInt()) {
+                possibleMoves.remove("up");
+            }
+        });
     }
 
     public void avoidWalls(JsonNode head, int height, int width, ArrayList<String> possibleMoves) {
